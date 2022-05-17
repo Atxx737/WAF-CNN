6d6ef7fc9eb6ba2b93ec5cf3bbfeeac4de7d0d79
SonNH <sonnh.uit@gmail.com>
2022-05-17 14:03:32 +0700

    add readme, complete function

--- dictionaryGenerate.py
+++ dictionaryGenerate.py
@@ -1,98 +1,109 @@
+#package
+
 import linecache
 import json
-# def lineIsPostData(line):
+
+#Define env
+
 DATASET_FILE='anomalousTrafficTest.txt'
 DICTIONARY_FILE='dictionary.json'
-CLEANED_DATASET_FILE='cleanedDataset.txt'
+DICTIONARY_BACKUP='dictionary-backup.json'
 FULL_PAYLOAD_FILE='payload.txt'
+
+# Filter payload from dataset
 def cleanDataSet(dataFile):
     dataSet = open(dataFile, 'r')
-    cleanedDataset = open(CLEANED_DATASET_FILE, 'w')
+    payload_file = open(FULL_PAYLOAD_FILE, 'w')
     lineIndex = 1
     for line in dataSet:
         if line.startswith("GET"):
-            cleanedDataset.write(line)
             lineIndex +=1
+            line = line.split(' ')
+            try:
+                payload_formated= line[1].split('?')
+                try:
+                    payload_file.write(payload_formated[1]+'\n')
+                except IndexError:
+                    pass
+            except IndexError:
+                pass
         elif line.startswith("POST"):
             postData = linecache.getline(dataFile, lineIndex + 14)
-            cleanedDataset.write(line[:-10]+"?" +postData)
+            payload_file.write(postData)
             lineIndex +=1
         else:
             lineIndex += 1
     dataSet.close()
-    cleanedDataset.close
+    payload_file.close()
 
-def write_json(new_data, filename, keyApend):
-    with open(filename,'r+') as file:
-        file_data = json.load(file)
-        file_data[keyApend].append(new_data)
-        file.seek(0)
-        json.dump(file_data, file, indent = 4)
 
-def checkPayloadIsExist(payload, dic_file):
-    dictionary = json.load(dic_file)
-    for key in dictionary["dictionary"][0]:
+# Check payload is exist in dictionary
+def checkPayloadIsExist(payload):
+    dict_file = open(DICTIONARY_FILE, 'r')
+    dict_data = json.loads(dict_file.read())
+    for key in dict_data["dictionary"]:
         if key["payload"] == payload:
-            return true
-        else:
-            return false
-def splitString(requestLink,charSplit):
-    temp_pay = requestLink.split(charSplit)
-    return temp_pay
-    # createDictionary(CLEANED_DATASET_FILE)
-    
-def getFullPayload():
-    cleanDataSet(DATASET_FILE)
-    dataSet = open(CLEANED_DATASET_FILE, 'r')
-    for line in dataSet:
-        temp_formater = splitString(line,'?')
-        payload_formated=''
-        try:
-            payload_formated=temp_formater[1]
-            if (payload_formated.endswith("HTTP/1.1\n")):
-                payload_formated = payload_formated[:-10]
-        except IndexError:
-            pass
-        payloadFile = open(FULL_PAYLOAD_FILE, 'a')
-        payloadFile.write(payload_formated)
-        payloadFile.close()
-    dataSet.close()
+            return True
+    return False
 
-# def createDictionary(fpayload):
-#     dictionary = json.load(DICTIONARY_FILE)
-#     dataSet = open(CLEANED_DATASET_FILE, 'r')
-#     for line in dataSet:
-#         payload = getFullPayload(line)
+# Plus frequency existed in dictionary
+def plusFrequency(payload_need_plus):
+    dict_file = open(DICTIONARY_FILE, 'r')
+    dict_data = json.loads(dict_file.read())
 
-#     for payload in dictionary["dictionary"][0]:
-#         if payload["payload"]
-#     appendData = {"dictionaryID": "1",
-#         "payload": "example",
-#         "dictionaryFrequency": 100
-#     }
-#     write_json(appendData,DICTIONARY_FILE,'dictionary')
-#     dataSet.close()
+    backup_dict = json.dumps(dict_data, indent = 4)
+    with open(DICTIONARY_BACKUP, "w") as outfile:
+        outfile.write(backup_dict)
 
+    for i, item in enumerate(dict_data['dictionary']):
+        if (item['payload'] == payload_need_plus):
+            item['dictionaryFrequency'] = item['dictionaryFrequency']+1
+            new_dictitonary = json.dumps(dict_data, indent = 4)
+            with open(DICTIONARY_FILE, "w") as outfile:
+                outfile.write(new_dictitonary)
+    dict_file.close()
+
+# Append new payload if it not exist
+def appendDictionary(append_data):
+    dict_file = open(DICTIONARY_FILE, 'r')
+    dict_data = json.loads(dict_file.read())
+
+    backup_dict = json.dumps(dict_data, indent = 4)
+    with open(DICTIONARY_BACKUP, "w") as outfile:
+        outfile.write(backup_dict)
+
+    with open(DICTIONARY_FILE,'r+') as file:
+        dict_data = json.load(file)
+        dict_data["dictionary"].append(append_data)
+        file.seek(0)
+        json.dump(dict_data, file,indent = 4)
+    dict_file.close()
+
+# Create Dicitonary Function    
+def createDictionary():
+    payload_data = open(FULL_PAYLOAD_FILE, 'r')
+    for line in payload_data:
+        line = line.split('&')
+        for item in line:
+            item = item.strip('\n')
+            if checkPayloadIsExist(item) == True:
+                plusFrequency(item)
+            else:
+                dict_file = open(DICTIONARY_FILE, 'r')
+                dict_data = json.loads(dict_file.read())
+                dict_len = len(dict_data['dictionary'])
+                maxid = dict_data['dictionary'][dict_len-1]['dictionaryID']+1
+                data = {
+                        "dictionaryID": maxid,
+                        "payload": item,
+                        "dictionaryFrequency": 1
+                    }
+                appendDictionary(data)
+                dict_file.close()
 
 #Main function
 def main():
     cleanDataSet(DATASET_FILE)
-    getFullPayload()
-    # createDictionary(CLEANED_DATASET_FILE)
-
+    createDictionary()
     
 main()
-# dataSet = open(CLEANED_DATASET_FILE, 'r')
-# for line in dataSet:
-#     payload = getFullPayload(line)
-#     temp_formater = splitString(line,'?')
-#     payload_formated=''
-#     try:
-#         payload_formated=temp_formater[1]
-#         if (payload_formated.endswith("HTTP/1.1\n")):
-#             payload_formated = payload_formated[:-10]
-#         print(payload_formated)
-#     except IndexError:
-#         pass
-
-# dataSet.close()
